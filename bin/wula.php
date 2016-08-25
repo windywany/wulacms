8249fffaebd1dd626905d512f86358bada115efb
windywany <windywany@163.com>
2016-08-25 10:55:05 +0800

    use absolute path of bootstrap.php file

--- bin/wula.php
+++ bin/wula.php
@@ -1,8 +1,9 @@
 <?php
+define ( 'WEB_ROOT', dirname ( __DIR__ ) . DIRECTORY_SEPARATOR );
 @ob_end_clean ();
 $appid = rand ( 1, 10000 );
 echo "\tgenerated appid: ", $appid, "\n";
 flush ();
-$content = file_get_contents ( 'bootstrap.php' );
+$content = file_get_contents ( WEB_ROOT . 'bootstrap.php' );
 $content = str_replace ( "basename ( WEB_ROOT )", "'A$appid')", $content );
-file_put_contents ( 'bootstrap.php', $content );
+file_put_contents ( WEB_ROOT . 'bootstrap.php', $content );
