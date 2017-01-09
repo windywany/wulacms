57e274db6d229195977770aabd5c0534997113fa
windywany <windywany@163.com>
2017-01-09 11:44:52 +0800

    添加接口check

--- modules/pay/classes/PayRestService.php
+++ modules/pay/classes/PayRestService.php
@@ -20,10 +20,10 @@ class PayRestService {
 	/**
 	 * 支付请求.
 	 *
-	 * @param array  $param
-	 *              [orderid=>'订单编号'
-	 *              paychannel=>'支付方式'
-	 *              ]
+	 * @param array  $param 参数
+	 *                      orderid=>'订单编号'
+	 *                      paychannel=>'支付方式'
+	 *
 	 * @param string $key
 	 * @param string $secret
 	 *
@@ -38,21 +38,35 @@ class PayRestService {
 	/**
 	 * 创建一个支付订单，并返回订单号
 	 *
-	 * @param array  $param
-	 *                ['order_type'=>'业务订单类型,必填',
-	 *                'amount'=>'金额，orderid为空时必填',
-	 *                'subject'=>'项目，orderid为空时必填',
-	 *                'mid'=>'用户编号,orderid为空时必真',
-	 *                'device'=>'设备，可选'
-	 *                'channel'=>'推广渠道，可选',
-	 *                'orderid'=>'业务订单编号，amount为空时，必填'
-	 *                ]
+	 * @param array  $param 参数
+	 *                      'order_type'=>'业务订单类型,必填',
+	 *                      'amount'=>'金额，orderid为空时必填',
+	 *                      'subject'=>'项目，orderid为空时必填',
+	 *                      'mid'=>'用户编号,orderid为空时必真',
+	 *                      'device'=>'设备，可选'
+	 *                      'channel'=>'推广渠道，可选',
+	 *                      'orderid'=>'业务订单编号，amount为空时，必填'
+	 *
 	 * @param string $key
 	 * @param string $secret
 	 *
 	 * @return array
 	 */
 	public function rest_post_create_order($param, $key, $secret) {
+		return [];
+	}
+
+	/**
+	 * 支付复核.
+	 *
+	 * @param array  $param 参数：
+	 *                      'orderid'=>'订单编号'
+	 * @param string $key
+	 * @param string $secret
+	 *
+	 * @return array
+	 */
+	public function rest_get_check($param, $key, $secret) {
 		return [];
 	}
 }
